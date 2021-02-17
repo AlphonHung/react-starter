dc78afac7a17a2b7015d74eb4eb5dd6684218836
Desmond Hung <alphon.hung@gmail.com>
2021-02-17 10:55:19 +0800

    [Add] bootstrap

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import '~/assets/scss/views/HomeView.scss';
+import { Button } from 'react-bootstrap';
 
 const HomeView = () => {
     const { t } = useTranslation();
     return (
         <div className="home">
             <h1>{t("view.home.welcome")}</h1>
+            <Button className="primary">Bootstrap Button</Button>
+            <button>Normal Button</button>
         </div>
     );
 }
